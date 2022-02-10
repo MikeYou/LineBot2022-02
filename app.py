0db2156d39962a8cbbf9fe8000a45a14842e4e95
MikeYou <96813411+MikeYou@users.noreply.github.com>
2022-02-10 14:34:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -202,6 +202,11 @@ def getCallCarMessage(data):
 
 def getPlayStickerMessage():
     message = dict()
+    message = {"type": "sticker",
+  "packageId": "446",
+  "stickerId": "1988"}
+  
+
     return message
 
 
